257f4a645a6ae493fd6e85902b100c7e8c6061c1
Tom Fox <tomfox747@gmail.com>
2021-01-01 17:33:37 +0000

    working

--- routers/data_post_router.js
+++ routers/data_post_router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const data_post_service = require('../services/data_post_service');
 
 router.post('/post_building_data',(req,res) =>{
+    console.log('hit post building');
     const response = data_post_service.post_building_data();
     res.send(response);
 })
