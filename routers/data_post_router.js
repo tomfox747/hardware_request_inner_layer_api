b0dc6549b895ec2091c8de13f5bb4300ff9a77cb
Tom Fox <tomfox747@gmail.com>
2021-02-07 15:59:52 +0000

    implement increment and decrement functionality

--- routers/data_post_router.js
+++ routers/data_post_router.js
@@ -4,10 +4,10 @@ const data_post_service = require('../services/data_post_service');
 const {getError, generateReturnError} = require('../utilities/errorHandler')
 const fileName = 'data_post_router';
 
-router.post('/post_building_data',(req,res) =>{
+router.post('/post_building_data', async (req,res) =>{
     try{
         let buildingObj = req.body
-        const response = data_post_service.post_building_data(buildingObj);
+        const response = await data_post_service.post_building_data(buildingObj);
         res.send(response);
     }catch(e){
         console.log(e)
@@ -17,10 +17,10 @@ router.post('/post_building_data',(req,res) =>{
     }
 })
 
-router.post('/post_room_data',(req,res) =>{
+router.post('/post_room_data', async (req,res) =>{
     try{
         let roomObj = req.body
-        const response = data_post_service.post_room_data(roomObj);
+        const response = await data_post_service.post_room_data(roomObj);
         res.send(response);    
     }catch(e){
         const err = getError(e,e.code, fileName, 'post_room_data');
@@ -29,14 +29,40 @@ router.post('/post_room_data',(req,res) =>{
     }
 })
 
-router.post('/post_sensor_data',(req,res) =>{
+router.post('/post_sensor_data', async (req,res) =>{
     try{
         let sensorObj = req.body
-        const response = data_post_service.post_sensor_data(sensorObj);
+        const response = await data_post_service.post_sensor_data(sensorObj);
         res.send(response);
     }catch(e){
         const err = getError(e,e.code, fileName, 'post_sensor_data');
-        const returnErrror = generateReturnError(err);
+        const returnError = generateReturnError(err);
+        res.send(returnError)
+    }
+})
+
+router.post('/increment_room_population', async (req,res) =>{
+    try{
+        let buildingId = req.body.buildingId
+        let roomId = req.body.roomId
+        const response = await data_post_service.increment_room_population(buildingId, roomId)
+        res.send(response)
+    }catch(e){
+        const err = getError(e,e.code,fileName,'increment_room_population')
+        const returnError = generateReturnError(err)
+        res.send(returnError)
+    }
+})
+
+router.post('/decrement_room_population',async (req,res) =>{
+    try{
+        let buildingId = req.body.buildingId
+        let roomId = req.body.roomId
+        const response = await data_post_service.decrement_room_population(buildingId,roomId)
+        res.send(response)
+    }catch(e){
+        const err = getError(e,e.code,fileName,'decrement_room_population')
+        const returnError = generateReturnError(err)
         res.send(returnError)
     }
 })
--- services/data_post_service.js
+++ services/data_post_service.js
@@ -6,8 +6,7 @@ const fileName = 'data_post_service';
 
 const Post_building_data = async (building) =>{
     try{
-        let model = new buildingModel(building)
-        await model.save()
+        await buildingModel.updateOne({Id:building.Id},building)
         return {status:200,data:'building data posted'}
     }catch(e){
         return getError(e,e.code,fileName, 'post_building_data');
@@ -16,26 +15,61 @@ const Post_building_data = async (building) =>{
 
 const Post_room_data = async (room) =>{
     try{
-        let model = new roomModel(room)
-        await model.save()
+        await roomModel.updateOne({Building:buildingId,RoomId:room.Id}, room)
         return {status:200,data:'room data posted'};
     }catch(e){
         return getError(e,e.code, fileName, 'post_room_data');
     }
 }
 
-const Post_sensor_data = async (sensor) =>{
+const Post_sensor_data = async (sensor, buildingId) =>{
     try{
-        let model = new sensorModel(sensor)
-        await model.save()
+        await sensorModel.updateOne({Building:buildingId, sensorId:sensor.sensorId},sensor)
         return {status:200, data:'sensor data posted'};
     }catch(e){
         return getError(e,e.code,fileName, 'post_sensor_data');
     }
 }
 
+const Increment_room_population = async(buildingId, roomId) =>{
+    try{
+        let room = await roomModel.findOne({Building:buildingId, RoomId:roomId}).lean().exec()
+        let newPopulation = Number(room.CurrentPopulation) + 1
+        room.CurrentPopulation = String(newPopulation)
+        await roomModel.updateOne({Building:buildingId, RoomId:roomId}, room)
+        let building = await buildingModel.findOne({Id:buildingId}).lean().exec()
+        newPopulation = Number(building.CurrentPopulation) + 1
+        building.CurrentPopulation = newPopulation
+        await buildingModel.updateOne({Id:buildingId},building)
+        return {status:200, data:'increment successful'};
+    }catch(e){
+        return getError(e,e.code,fileName,'increment_room_population')
+    } 
+}
+
+const Decrement_room_population = async(buildingId,roomId) =>{
+    try{
+        let room = await roomModel.findOne({Building:buildingId, RoomId:roomId}).lean().exec()
+        if (Number(room.CurrentPopulation) < 1){
+            return {status:200, data:'population 0'}
+        }
+        let newPopulation = Number(room.CurrentPopulation) - 1
+        room.CurrentPopulation = String(newPopulation)
+        await roomModel.updateOne({Building:buildingId, RoomId:roomId}, room)
+        let building = await buildingModel.findOne({Id:buildingId}).lean().exec()
+        newPopulation = Number(building.CurrentPopulation) - 1
+        building.CurrentPopulation = newPopulation
+        await buildingModel.updateOne({Id:buildingId},building)
+        return {status:200, data:'decrement successful'};
+    }catch(e){
+        return getError(e,e.code,fileName,'increment_room_population')
+    } 
+}
+
 module.exports ={
     post_building_data: Post_building_data,
     post_room_data: Post_room_data,
-    post_sensor_data: Post_sensor_data
+    post_sensor_data: Post_sensor_data,
+    increment_room_population:Increment_room_population,
+    decrement_room_population:Decrement_room_population
 }
