54148ec0d23c16b1994c043ae47194afa954feba
tomfox747 <thomasfox747@gmail.com>
2021-02-19 12:36:23 +0000

    added and fixed auth

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const data_post_router = require('./routers/data_post_router');
 //initialise the hist data service to begin sending the data to the hist database
 const historical_data_service = require('./utilities/historical_data_service.js')
 
+const authOrigin = require('./middleware/authentication')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/healthCheck', healthCheck_router);
-app.use('/inner/data', data_post_router);
+app.use('/inner/data', authOrigin, data_post_router);
 
 module.exports = app;
